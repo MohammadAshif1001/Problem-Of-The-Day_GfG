b65b3f3a8a383ac05d157b6140427b82317ffeff
Mohammad Ashif <72151379+MohammadAshif1001@users.noreply.github.com>
2023-08-27 22:42:11 +0530

    Create 27-Aug-Reverse-a-String.java

---
+++ Aug2023/27-Aug-Reverse-a-String.java
@@ -0,0 +1,14 @@
+//Today Very easy Question Link - https://practice.geeksforgeeks.org/problems/reverse-a-string/1
+class Reverse
+{
+    public static String reverseWord(String str)
+    {
+        // Reverse the string str
+        StringBuilder revStr = new StringBuilder("");
+        int n=str.length()-1;
+        for(int i=n; i>=0; i--){
+            revStr.append(str.charAt(i));
+        }
+        return revStr.toString();
+    }
+}
